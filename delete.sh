bfbfd4940783968bf6333dbd17d9b005ecc42de6
NactEunS <eweiyan05947809@126.com>
2021-04-14 14:11:52 +0800

    更新 delete.sh

--- delete.sh
+++ delete.sh
@@ -1,5 +1,5 @@
-container=$(docker ps -a -f name=springbootdemo -q)
+container=$(docker ps -a -f name=$STAGE_NAME -q)
 echo $container
 if [ -n "$container" ] ;then
- docker rm -f $(docker ps -a -f name=springbootdemo -q)
+ docker rm -f $(docker ps -a -f name=$STAGE_NAME -q)
 fi
