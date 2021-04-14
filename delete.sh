85439dbcb9474da5f76b38fe2cf255e6dca3565f
NactEunS <eweiyan05947809@126.com>
2021-04-14 14:28:03 +0800

    更新 delete.sh

--- delete.sh
+++ delete.sh
@@ -1,4 +1,5 @@
 container=$(docker ps -a -f name=$STAGE_NAME -q)
+echo asdfasdfasdfasdfghjklgfdfghjklkjhgh--------$STAGE_NAME
 echo $container
 if [ -n "$container" ] ;then
  docker rm -f $(docker ps -a -f name=$STAGE_NAME -q)
